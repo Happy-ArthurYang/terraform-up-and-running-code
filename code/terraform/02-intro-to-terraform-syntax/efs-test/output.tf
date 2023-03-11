d2f6d026af7ae885c08a7ac66129cda54af69ece
Yang Arthur CNBC <arthur.yang@syngenta.com>
2023-03-11 08:04:51 +0800

    modify output vars

--- code/terraform/02-intro-to-terraform-syntax/efs-test/output.tf
+++ code/terraform/02-intro-to-terraform-syntax/efs-test/output.tf
@@ -1,6 +1,6 @@
-output dbefsarn{
-    value = aws_efs_file_system.efs["DECODREDbEfs-dev"].arn    
+output efsnames{
+    value = tolist(keys(aws_efs_file_system.efs)[*])  
 }
-output mediaefsearn{
-    value = aws_efs_file_system.efs["DECODRMediaEfs-dev"].arn
+output efsearns{
+    value = values(aws_efs_file_system.efs)[*].arn
 }
