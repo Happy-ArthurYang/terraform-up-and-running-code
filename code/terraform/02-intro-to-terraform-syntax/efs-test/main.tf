6e625ca629406b519f05bf1c475c70d297fcc963
Yang Arthur CNBC <arthur.yang@syngenta.com>
2023-03-13 09:14:51 +0800

    dynamic purpose tags for efs

--- code/terraform/02-intro-to-terraform-syntax/efs-test/main.tf
+++ code/terraform/02-intro-to-terraform-syntax/efs-test/main.tf
@@ -23,7 +23,11 @@ resource "aws_efs_file_system" "efs" {
    encrypted = "true"
  tags = {
      Name = each.key
+     Purpose =startswith(each.key,"decodr-db.sqlite3")?"Shared volumes for decodr application db file for ${var.env}.":"Shared volumes for decodr application analysis media files for ${var.env}"
    }
+    lifecycle {
+  prevent_destroy =true
+ } 
  }
 
 resource "aws_efs_mount_target" "efs-mt4DB" {
@@ -31,12 +35,18 @@ resource "aws_efs_mount_target" "efs-mt4DB" {
    file_system_id  = aws_efs_file_system.efs["decodr-db.sqlite3-${var.env}"].id
    subnet_id = var.subnet_ids[count.index]
    security_groups = ["sg-040c4ead4674d622f"]
+    lifecycle {
+  prevent_destroy =true
+ } 
  }
  resource "aws_efs_mount_target" "efs-mt4Media" {
    count = length(var.subnet_ids)
    file_system_id  = aws_efs_file_system.efs["decodr-media-${var.env}"].id
    subnet_id = var.subnet_ids[count.index]
    security_groups = ["sg-040c4ead4674d622f"]
+    lifecycle {
+  prevent_destroy =true
+ } 
  }
 
 # output dbefsid{
